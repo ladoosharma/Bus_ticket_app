15c634856e9e4c387c0e307d7d27c7f6877d72ad
Gaurav <gs97197@gmail.com>
2020-05-19 11:10:53 +0530

    commited

--- force-app/main/default/aura/Ticket_Booking_component/Ticket_Booking_componentController.js
+++ force-app/main/default/aura/Ticket_Booking_component/Ticket_Booking_componentController.js
@@ -6,13 +6,17 @@
             if (response.getState() === "SUCCESS") {
                 var value = response.getReturnValue();
                 var seatList =[];
+                var seatObj ={};
                 var listLen = value.seatsList.length;
-
+                for(var seat of value.seatsList){
+                    seatObj[seat.Id] = seat;
+                }
                 while(value.seatsList.length){
                     seatList.push(value.seatsList.splice(0, listLen/2))
                 }
+                component.set("v.seatMap", seatObj);
                 component.set("v.seatList", seatList);
-                component.set("v.userType", value.userInfo);
+                component.set("v.userType", value.userDetail);
             }
         });
         $A.enqueueAction(action);
@@ -20,6 +24,7 @@
 
     selectSeats: function (component, event, helper) {
         var source = event.getSource();
+        var seatCost = component.get("v.seatList")[0][0].Ticket_Price__c;
         var selectedSeats = component.get("v.selectedSeats");
         if(source.get("v.class") === "selected"){
             if(selectedSeats.includes(source.get("v.name"))){
@@ -33,6 +38,21 @@
                 component.set("v.selectedSeats", selectedSeats);
                 source.set("v.class", "selected");
             }
+        }
+        if(selectedSeats){
+            component.set("v.totalCost", (selectedSeats.length)*seatCost);
+        }
+    },
+    payAmount : function(component, event, helper){
+        var seatMap = component.get("v.seatMap");
+        var occupiedSeatDetail=[];
+
+        if(!component.get("v.selectedSeats")){
+            for(var item of component.get("v.selectedSeats")){
+                occupiedSeatDetail.push(seatMap[item]);
+            }
+        }else{
+            //thow message that please select seats
         }
     }
 })
